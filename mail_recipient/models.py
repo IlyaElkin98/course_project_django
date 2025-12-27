2efdb5e80d6034cbda12cda7f2b589a2b4dac356
ilya <ilifud98@mail.ru>
2025-12-28 00:19:21 +0700

    refactor(mail_recipient.models):add models.py

--- mail_recipient/models.py
+++ mail_recipient/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+email = models.CharField(verbose_name='Электронная почта', help_text='Введите электронную почту')
+full_name = models.CharField(verbose_name="Ф.И.О.", help_text='Введите ФИО')
+comment = models.TextField(verbose_name='Комментарий')
+
