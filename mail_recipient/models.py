3b7a1e8c8f406ee1d5710b5e9a302e571d390ed6
ilya <ilifud98@mail.ru>
2025-12-28 00:35:58 +0700

    refactor(mail_recipient.models):edit

--- mail_recipient/models.py
+++ mail_recipient/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
-email = models.CharField(verbose_name='Электронная почта', help_text='Введите электронную почту')
-full_name = models.CharField(verbose_name="Ф.И.О.", help_text='Введите ФИО')
-comment = models.TextField(verbose_name='Комментарий')
+email = models.CharField(
+    verbose_name='Электронная почта',
+    help_text='Введите электронную почту'
+)
+
+full_name = models.CharField(
+    verbose_name="Ф.И.О.",
+    help_text='Введите ФИО'
+)
+
+comment = models.TextField(
+    verbose_name='Комментарий'
+)
 
