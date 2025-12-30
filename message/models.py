308d3e2cd4f8d495d357c79d0faf20aa44254c5a
ilya <ilifud98@mail.ru>
2025-12-30 17:33:10 +0700

    refactor(message.models):add models.py

--- message/models.py
+++ message/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    topic_letter = models.CharField(
+        verbose_name='Тема письма',
+        help_text='Введите тему письма'
+    )
+
+    body_letter = models.TextField(
+        verbose_name='Тело письма',
+        help_text='Содержание письма'
+    )
+
