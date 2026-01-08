abe4045533b71ded47ec8c18df6629bf4451db1c
ilya <ilifud98@mail.ru>
2026-01-08 22:10:05 +0700

    refactor(migrate):add migrate 2

---
+++ newsletter/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from newsletter.models import Newsletter
+
+
+class NewsletterForm(forms.ModelForm):
+    class Meta:
+        model = Newsletter
+        fields = ["start_time", "end_time", "status", "message", "recipients"]
