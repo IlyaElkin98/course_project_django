e80420f23e05238ac59f3a4fa934c69d02479b8f
ilya <ilifud98@mail.ru>
2026-01-11 22:03:30 +0700

    refactor(newsletter.views.py):edit views.py

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render, get_object_or_404
-from django.urls import reverse, reverse_lazy
+import os
 
+from django.core.mail import send_mail
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse, reverse_lazy
+from django.utils import timezone
+from dotenv import load_dotenv
+
+from mail_recipient.models import Recipient
 from .forms import NewsletterForm
 from .models import Newsletter
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib import messages
+
+load_dotenv()
 
 
 class NewsletterCreateView(CreateView):
@@ -45,3 +54,33 @@ class NewsletterDeleteView(DeleteView):
     template_name = "newsletter/confirm_delete_newsletter.html"
     success_url = reverse_lazy("newsletter:newsletters_list")
 
+
+from django.shortcuts import get_object_or_404
+
+def send_newsletter(request, pk):
+    newsletter = get_object_or_404(Newsletter, pk=pk)
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST, instance=newsletter)
+        if form.is_valid():
+            newsletter = form.save(commit=False)
+            now = timezone.now()
+            if newsletter.start_time <= now <= newsletter.end_time:
+                recipients = Recipient.objects.filter(newsletters=newsletter)
+                for recipient in recipients:
+                    try:
+                        send_mail(
+                            newsletter.subject,
+                            newsletter.content,
+                            os.getenv('EMAIL'),
+                            [recipient.email],
+                            fail_silently=False,
+                        )
+                        messages.success(request, f"Письмо успешно отправлено {recipient.email}.")
+                    except Exception as e:
+                        messages.error(request, f"Ошибка отправки на {recipient.email}: {str(e)}.")
+                return redirect('newsletter.views')
+            else:
+                messages.error(request, "Текущее время находится вне диапазона отправки.")
+    else:
+        form = NewsletterForm(instance=newsletter)
+    return render(request, 'newsletter/send_newsletter.html', {'form': form, 'newsletter': newsletter})
