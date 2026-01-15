aceccf17f2f2bc3d5dd1b700218828c375ab7eaa
ilya <ilifud98@mail.ru>
2026-01-15 20:51:55 +0700

    refactor(user.views):add views.py

--- user/views.py
+++ user/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+import secrets
+from django.contrib.auth import logout
+from django.core.mail import send_mail
+from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView
 
-# Create your views here.
+from user.forms import UserRegisterForm
+from user.models import User
+from config.settings import EMAIL_HOST_USER
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('')
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+        host = self.request.get_host()
+        url = f'http://{host}/user/email-confirm/{token}/'
+        send_mail(
+            subject='Подтверждение почты',
+            message=f'Привет, перейди по ссылки для подтверждения регистрации {url}',
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email]
+        )
+        return super().form_valid(form)
+
+
+
+def email_varification(request, token):
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.save()
+    return redirect(reverse(''))
+
+
+def custom_logout(request):
+    logout(request)
+    return redirect('')
