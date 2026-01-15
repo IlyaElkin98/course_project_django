075ff68a5be83f0bb6759657f263a4c8f63b3aa6
ilya <ilifud98@mail.ru>
2026-01-16 00:44:41 +0700

    refactor(config.urls):add path user

--- config/urls.py
+++ config/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("recipient/", include(("mail_recipient.urls", "mail_recipient"), namespace="mail_recipient")),
     path("message/", include(("message.urls", "message"), namespace="message")),
     path("newsletter/", include(("newsletter.urls", "newsletter"), namespace="newsletter")),
-    path("", include(('message.urls', 'main'), namespace='main'))
+    path("", include(('message.urls', 'main'), namespace='main')),
+    path('user/', include(('user.urls', 'user'), namespace='user'))
 ]
